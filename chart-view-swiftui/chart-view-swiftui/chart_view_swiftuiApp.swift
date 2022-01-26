9754028053a74c3c66075f8bda6d572a28c1f28f
foggydawning <foggydawning@gmail.com>
2022-01-26 18:05:26 +0300

    create xcodeproject

---
+++ chart-view-swiftui/chart-view-swiftui/chart_view_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chart_view_swiftuiApp.swift
+//  chart-view-swiftui
+//
+//  Created by Илья Чуб on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct chart_view_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
