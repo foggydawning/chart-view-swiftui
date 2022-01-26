8f2c4f22e929d447363af6947c3e11d0f183edfb
foggydawning <foggydawning@gmail.com>
2022-01-26 18:36:06 +0300

    create files for the chart MVVM

---
+++ chart-view-swiftui/chart-view-swiftui/View/chartView.swift
@@ -0,0 +1,8 @@
+//
+//  chartView.swift
+//  chart-view-swiftui
+//
+//  Created by Илья Чуб on 26.01.2022.
+//
+
+import Foundation
---
+++ chart-view-swiftui/chart-view-swiftui/ViewModel/chartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  chartViewModel.swift
+//  chart-view-swiftui
+//
+//  Created by Илья Чуб on 26.01.2022.
+//
+
+import Foundation
