781fb91299ad2cc27ad10955d47ba163d27ff71b
foggydawning <foggydawning@gmail.com>
2022-01-30 21:28:17 +0300

    create CoordinateLineModel

--- chart-view-swiftui/chart-view-swiftui/Model/CoordinateLineModel.swift
+++ chart-view-swiftui/chart-view-swiftui/Model/CoordinateLineModel.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Илья Чуб on 30.01.2022.
 //
+import Foundation
+import CoreGraphics
 
+struct CoordinateLineModel {
+    var numberOfIntervals: Int = 0
+    
+    var startDate: Date? = nil
+    var endDate: Date? = nil
+    
+    var distanceBetweenColumns: CGFloat = 4
+    var colWidth: CGFloat = 5
+}
